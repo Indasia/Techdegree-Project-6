ed0f1ef8aaf0edf3acecb4908d7e65f17fb70fd6
Indasia Summerfield <Indasia@yahoo.com>
2019-03-27 18:50:25 -0700

    Initial commit

---
+++ public/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).foundation()
+
+
+
+// require express
+// const express = require("express");
+
+
+// call express
+// const app = express();
+
+
+// tell express to use pug
+// app.set("view engine", "pug");
+
+
+// get the root route
+/* app.get("/", (request, response) => {
+        response.send(" xyz ");
+}); */
+
+
+// the port to serve the application on
+// app.listen(3000);
