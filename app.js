bbca197d7ff2795ea0f9cef499671c4bcab6d984
Indasia Summerfield <Indasia@yahoo.com>
2019-04-02 13:22:21 -0700

    added more code to files

---
+++ app.js
@@ -0,0 +1,53 @@
+// require data.json
+const data = require("./data.json");
+
+// require express
+const express = require("express");
+
+const projects = data.projects;
+
+// call express
+const app = express();
+
+// setup middleware, tell express to use pug
+app.set("view engine", "pug");
+
+// use a static route and the express.static method to serve the static files located in the public folder
+app.use("/static", express.static("public"));
+
+//An "index" route (/) to render the "Home" page with the locals set to data.projects
+app.get("/", (req, res) => {
+        res.render("index",  data.projects );
+});
+
+//An "about" route (/about) to render the "About" page
+app.get("/about", (req, res) => {
+        res.render("about");
+});
+
+//Dynamic "project" routes (/project or /projects) based on the id of the project that render a customized version of the Pug project 
+app.get("/projects/:id", (req, res) => {
+        res.render()
+});
+//template to show off each project. Which means adding data, or "locals", as an object that contains data to be passed to the Pug template.
+
+
+// start the server, the port to serve the application on
+app.listen(3000, () => {
+        // log a string to the console that says which port the app is listening to.
+        console.log("Your application is now connected to port 3000!")
+});
+
+app.use((req, res, next) => {
+        const err = new Error("Congratulations, you broke the internet.");
+        console.log("Sorry, this page doesn't exist!");
+        err.status = 404;
+        next(err);
+});
+
+// check for errors and then when that is complete move on
+app.use((err, req, res, next) => {
+        res.locals.error = err;
+        res.status(err.status)
+        res.render("error");
+});
